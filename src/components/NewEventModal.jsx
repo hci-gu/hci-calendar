5f3ebf4198c13b5842b0ad045148a66758c06b00
Styrbjörn Nordberg <styris.n@gmail.com>
2024-02-06 09:58:35 +0100

    kinda can add new events via the ui

---
+++ src/components/NewEventModal.jsx
@@ -0,0 +1,112 @@
+import { Button, Flex, Group, Modal, TextInput, Textarea } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { DateInput } from '@mantine/dates'
+import '@mantine/dates/styles.css'
+import { useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+
+const NewEventModal = () => {
+    const [opend, { open, close }] = useDisclosure(false)
+    const [formData, setFormData] = useState({
+        title: '',
+        startDate: '',
+        endDate: '',
+        description: '',
+    })
+    const supabase = createClient(
+        import.meta.env.VITE_SUPABASE_URL,
+        import.meta.env.VITE_SUPABASE_KEY
+    )
+
+    const insertSupabase = async () => {
+        const { error } = await supabase.from('events').insert({
+            start: formData.startDate,
+            end: formData.endDate,
+            title: formData.title,
+            description: formData.description,
+        })
+        if (error) {
+            console.log(error)
+        }
+        setFormData({
+            title: '',
+            startDate: '',
+            endDate: '',
+            description: '',
+        })
+    }
+
+    return (
+        <>
+            <Modal title="New Event" opened={opend} onClose={close} centered>
+                <form>
+                    <Flex align="center" direction="column" w="100%" gap={16}>
+                        <TextInput
+                            w="100%"
+                            label="Event Title"
+                            withAsterisk
+                            placeholder="Title"
+                            value={formData.title}
+                            onChange={(e) => {
+                                setFormData({
+                                    ...formData,
+                                    title: e.target.value,
+                                })
+                            }}
+                        />
+                        <Group justify="space-between" w="100%">
+                            <DateInput
+                                label="Start date"
+                                placeholder="2024-01-01"
+                                valueFormat="YYYY-MM-DD"
+                                withAsterisk
+                                value={formData.startDate}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        startDate: e,
+                                    })
+                                }}
+                            />
+                            <DateInput
+                                label="End Date"
+                                placeholder="2024-01-01"
+                                valueFormat="YYYY-MM-DD"
+                                withAsterisk
+                                value={formData.endDate}
+                                onChange={(e) => {
+                                    setFormData({
+                                        ...formData,
+                                        endDate: e,
+                                    })
+                                }}
+                            />
+                        </Group>
+                        <Textarea
+                            w="100%"
+                            label="Event Description"
+                            placeholder="Description"
+                            value={formData.description}
+                            onChange={(e) => {
+                                setFormData({
+                                    ...formData,
+                                    description: e.target.value,
+                                })
+                            }}
+                        />
+                        <Button type="button" onClick={insertSupabase}>
+                            submit
+                        </Button>
+                    </Flex>
+                </form>
+            </Modal>
+            <div>
+                <Button onClick={open} variant="filled">
+                    New Event
+                </Button>
+            </div>
+        </>
+    )
+}
+
+export default NewEventModal
