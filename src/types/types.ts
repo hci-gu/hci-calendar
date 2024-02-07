9621e6aeae37424af6b75258f87b3689c37b68d4
Josef Forkman <strand.vatten@outlook.com>
2024-02-07 14:20:58 +0100

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,12 +2,20 @@ import { Database } from "@/supabase/supabase";
 
 export type EventType = Database['public']['Tables']['events']['Row']
 
+export type sizeType = {
+    width: number
+    height: number
+}
+
 export type EventAtom = EventType & {
     position: { x: number; y: number }
-    size: { width: number; height: number }
+    size: sizeType
 }
 export type updateType = {
     id: number
     position: { x: number; y: number }
-    size: { width: number; height: number }
+    size: sizeType
 }
+
+/* https://stackoverflow.com/a/77346296 */
+export type SetAtom<Args extends any[], Result> = (...args: Args) => Result;
