76440b8e9e3c4c23f88fdc483439748b97a4feb7
JosefForkman <strand.vatten@outlook.com>
2024-02-14 17:36:49 +0100

    move deadlines Zod object to separate file

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,32 +1,32 @@
-import { Card, Flex, Text } from '@mantine/core'
-import { positionToDate } from '../lib/utils'
+import { Card, Flex, Paper, Text, Title } from '@mantine/core'
 import { EventAtom } from '../types/types'
-import { useViewportSize } from '@mantine/hooks'
+import { deadlinesZod } from '../types/zod'
+import { IconAdjustments, IconBell, IconFolder } from '@tabler/icons-react';
+import moment from 'moment';
 
 const EventCard = ({ event }: { event: EventAtom }) => {
-    const viewport = useViewportSize()
+    const deadlines = deadlinesZod.array().parse(event.deadlines);
+
     return (
-        <Card shadow="sm" withBorder style={{ height: '100%' }} radius="md">
-            <Card.Section p={4} withBorder bg="orange">
-                <Flex justify="space-between">
-                    <Text size="xs" fw="bold">
-                        {positionToDate(
-                            event.position.x,
-                            viewport.width
-                        ).format('MMM DD')}
-                    </Text>
-                    <Text size="xs" fw="bold">
-                        {positionToDate(
-                            event.position.x + event.size.width,
-                            viewport.width
-                        ).format('MMM DD')}
-                    </Text>
+        <Paper shadow="sm" radius="md">
+            <Flex direction="column" justify="end" gap="sm">
+                <Title c={'HCI-Green.8'} order={2}>
+                    {event.title}
+                </Title>
+                <Flex align="end" direction="column" bg='HCI-Green.4' p="sm" >
+
+                    <Flex>
+                        <Text c={'HCI-Green.8'}>
+                            <IconBell />
+                            {deadlines[0].name}
+                        </Text>
+                        <Text c={'HCI-Green.8'}>
+                            {moment(deadlines[0].timestamp).format('D/M')}
+                        </Text>
+                    </Flex>
                 </Flex>
-            </Card.Section>
-            <Card.Section p={8} withBorder bg="red">
-                <Text size="sm">{event.title}</Text>
-            </Card.Section>
-        </Card>
+            </Flex>
+        </Paper>
     )
 }
 
---
+++ src/types/zod.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod'
+
+export const deadlinesZod = z.object({
+    name: z.string().min(3),
+    timestamp: z.string().nullable(),
+})
