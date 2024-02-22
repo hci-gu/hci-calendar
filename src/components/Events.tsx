a502af31a9127539b0a3ed868b2a8433b43303ed
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2024-02-22 14:20:42 +0100

    zIndex based on sort of events

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Rnd } from 'react-rnd'
 import EventCard from './EventCard'
-import { positionAndWidthForDates, sortEventsIntoRows, getColor } from '../lib/utils'
+import {
+    positionAndWidthForDates,
+    sortEventsIntoRows,
+    getColor,
+} from '../lib/utils'
 import { useViewportSize } from '@mantine/hooks'
 import { useEvents } from '../lib/state'
 import { EventType, Row } from '../types/types'
@@ -10,6 +14,7 @@ import { DeadlineFormType } from '../types/zod'
 const Events = () => {
     const viewport = useViewportSize()
     const events = useEvents()
+
     const rows = sortEventsIntoRows({ events, viewportHeight: 2073 })
 
     return (
@@ -17,12 +22,17 @@ const Events = () => {
             {rows.map((row: Row) => {
                 return row.eventsInRow.map((event: EventType) => {
                     const [x, width] = positionAndWidthForDates(
-                        event.deadlines.map((d: DeadlineFormType) => d.timestamp),
+                        event.deadlines.map(
+                            (d: DeadlineFormType) => d.timestamp
+                        ),
                         viewport.width
                     )
 
                     return (
                         <Rnd
+                            style={{
+                                zIndex: -events.indexOf(event),
+                            }}
                             key={event.id}
                             position={{
                                 x,
@@ -32,13 +42,15 @@ const Events = () => {
                             enableResizing={false}
                             bounds="window"
                         >
-                            <EventCard event={event} width={width} color={getColor(event.type)} />
+                            <EventCard
+                                event={event}
+                                width={width}
+                                color={getColor(event.type)}
+                            />
                         </Rnd>
                     )
                 })
-            }
-            )
-            }
+            })}
         </>
     )
 }
