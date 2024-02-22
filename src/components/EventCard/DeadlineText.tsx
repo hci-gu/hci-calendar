4b2df153cf2aa34c9c10d92598fbdfc3136c8f98
Josef Forkman <strand.vatten@outlook.com>
2024-02-22 09:42:55 +0100

    Add conditional rendering for deadline text (dedline from now)

--- src/components/EventCard/DeadlineText.tsx
+++ src/components/EventCard/DeadlineText.tsx
@@ -5,6 +5,8 @@ import { IconBell } from "@tabler/icons-react";
 import moment from "moment";
 
 export default function DeadlineText({ event, color }: { event: DeadlineFormType, color: colorType }) {
+    const is14DaysFromNow = moment(event.timestamp).isBefore(moment().add(14, 'days'))
+
     return (
         <>
             <Flex h="100%" align="flex-end" direction="column" p="md">
@@ -15,9 +17,12 @@ export default function DeadlineText({ event, color }: { event: DeadlineFormType
                 <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
                     {moment(event.timestamp).format('MM/DD')}
                 </Text>
-                <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
-                    {moment(event.timestamp).fromNow()}
-                </Text>
+
+                {is14DaysFromNow &&
+                    <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
+                        {moment(event.timestamp).fromNow()}
+                    </Text>
+                }
             </Flex>
 
         </>
