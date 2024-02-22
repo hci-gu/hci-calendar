d89ed7d9cfa18e093c46ba91618cd31f097549ce
Josef Forkman <strand.vatten@outlook.com>
2024-02-22 13:09:08 +0100

    fix icon on deadline

--- src/components/EventCard/DeadlineText.tsx
+++ src/components/EventCard/DeadlineText.tsx
@@ -1,7 +1,9 @@
 import { colorType } from "@/src/lib/mantineConfig";
+import { getColor } from "@/src/lib/utils";
 import { DeadlineFormType } from "@/src/types/zod";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Flex, Text } from "@mantine/core";
-import { IconBell } from "@tabler/icons-react";
 import moment from "moment";
 
 export default function DeadlineText({ event, color }: { event: DeadlineFormType, color: colorType }) {
@@ -10,10 +12,12 @@ export default function DeadlineText({ event, color }: { event: DeadlineFormType
     return (
         <>
             <Flex h="100%" align="flex-end" direction="column" p="md">
-                <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
-                    <IconBell />
-                    {` ${event.name}`}
-                </Text>
+                <Flex align="center" gap="xs">
+                    <FontAwesomeIcon icon={faBell} color={`var(--mantine-color-${color}-8)`} />
+                    <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
+                        {event.name}
+                    </Text>
+                </Flex>
                 <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
                     {moment(event.timestamp).format('MM/DD')}
                 </Text>
