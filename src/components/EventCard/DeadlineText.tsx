b4aa83bac892530e07ec29e7ae647048add4947e
Josef Forkman <strand.vatten@outlook.com>
2024-02-22 14:11:04 +0100

    update font size

--- src/components/EventCard/DeadlineText.tsx
+++ src/components/EventCard/DeadlineText.tsx
@@ -3,7 +3,7 @@ import { getColor } from "@/src/lib/utils";
 import { DeadlineFormType } from "@/src/types/zod";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Flex, Text } from "@mantine/core";
+import { Flex, Text, Title } from "@mantine/core";
 import moment from "moment";
 
 export default function DeadlineText({ event, color }: { event: DeadlineFormType, color: colorType }) {
@@ -14,16 +14,16 @@ export default function DeadlineText({ event, color }: { event: DeadlineFormType
             <Flex h="100%" align="flex-end" direction="column" p="md">
                 <Flex align="center" gap="xs">
                     <FontAwesomeIcon icon={faBell} color={`var(--mantine-color-${color}-8)`} />
-                    <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
+                    <Title order={3} fz="xl" style={{ textWrap: 'nowrap' }} c={color + ".8"}>
                         {event.name}
-                    </Text>
+                    </Title>
                 </Flex>
-                <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
+                <Text style={{ textWrap: 'nowrap' }} size="lg" c={color + ".8"}>
                     {moment(event.timestamp).format('MM/DD')}
                 </Text>
 
                 {is14DaysFromNow &&
-                    <Text style={{ textWrap: 'nowrap' }} fw="bold" c={color + ".8"}>
+                    <Text style={{ textWrap: 'nowrap' }} size="lg" c={color + ".8"}>
                         {moment(event.timestamp).fromNow()}
                     </Text>
                 }
