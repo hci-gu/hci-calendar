750e9ae92fd50d0f351acd2b156b24401d186c52
JosefForkman <strand.vatten@outlook.com>
2024-02-23 11:01:29 +0100

    update height & color

--- src/components/TodayIndicator.tsx
+++ src/components/TodayIndicator.tsx
@@ -6,7 +6,7 @@ export const TodayIndicator = () => {
     const [x] = positionAndWidthForDates([new Date()], window.innerWidth);
     return (
         <>
-            <div style={{ position: "absolute", height: "100%", width: x, backdropFilter: "grayscale(1)", zIndex: 1000, borderRight: "5px solid red" }} />
+            <div style={{ position: "absolute", height: "calc(100% - 59px)", width: x, backdropFilter: "grayscale(1)", zIndex: 1000, borderRight: "3px solid var(--mantine-color-red-text)" }} />
             <Text size="xl" c="red" style={{ position: "absolute", left: 8 + x, zIndex: 1000 }} >{moment().format('DD')}</Text>
         </>
     );
