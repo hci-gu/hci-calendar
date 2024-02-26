95c013768456e87e69c3af27f2ab48ba4c11279a
Josef Forkman <strand.vatten@outlook.com>
2024-02-26 10:29:19 +0100

    add icon fild in db

---
+++ pocketbase/pb_migrations/1708939562_updated_events.js
@@ -0,0 +1,44 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("ff4qygim75cpw9n")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ryhdxopy",
+    "name": "icon",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("ff4qygim75cpw9n")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ryhdxopy",
+    "name": "icon",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
