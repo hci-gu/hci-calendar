5a27ca1fcb20d06fc794e30c7627c1eb5d5d8b13
Josef Forkman <strand.vatten@outlook.com>
2024-03-04 15:54:40 +0100

    Add TodayIndicator component to Edit and Index routes

--- src/routes/edit.lazy.tsx
+++ src/routes/edit.lazy.tsx
@@ -2,6 +2,7 @@ import { createLazyFileRoute } from '@tanstack/react-router'
 import Header from '../components/Header'
 import Events from '../components/Events'
 import AddEventButton from '../components/AddEventButton/AddEventButton'
+import { TodayIndicator } from '../components/TodayIndicator'
 
 export const Route = createLazyFileRoute('/edit')({
   component: Edit,
@@ -12,6 +13,7 @@ function Edit() {
     <>
       <Header />
       <Events />
+      <TodayIndicator />
       <AddEventButton />
     </>
   )
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import Events from '../components/Events'
 import Header from '../components/Header'
+import { TodayIndicator } from '../components/TodayIndicator'
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -11,6 +12,7 @@ function Index() {
     <>
       <Header />
       <Events />
+      <TodayIndicator />
     </>
   )
 }
